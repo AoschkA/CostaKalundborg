b2201e163c48e29c52d890c5d99fdc25795d5b04
Blackflunk <Alexander12350@hotmail.com>
2015-12-02 14:17:25 +0100

    Opdatering af invoicelogic

--- src/logic/InvoiceLogic.java
+++ src/logic/InvoiceLogic.java
@@ -1,29 +1,49 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import entity.invoice.Invoice;
+import entity.reservation.Reservation;
 
 public class InvoiceLogic {
 	ArrayList<Invoice> invoiceList;
+	
 	
 	public InvoiceLogic(){
 		
 	}
 	
 	public int makeInvoice(String string){
-		return 0;
+		String[] reservationIDs = string.split(",");
+		
+		for (int i=0;i<reservationIDs.length;i++){
+			for (i;)
+		}
+		ArrayList reservationIDsList= new ArrayList(Arrays.asList(reservationIDs));
+		
+		ArrayList<Reservation> reservationList= new ArrayList<Reservation>();
+		for(Reservation r : reservationIDs){
+		reservationList.add(
+		}
+		
+		
+		invoiceList.add(new Invoice(reservationIDsList))
 		
 	}
 	
 	public int makeInvoice(ArrayList <Invoice> invoiceList){
-		return 0;
-		
+		this.invoiceList = invoiceList;
+		invoiceList.
 	}
 	
 	public ArrayList<Invoice> getCustomerInvoices(String customerID){
-		for (Invoice e : invoiceList){
-			
+		for (Invoice invoice : invoiceList){
+			for(int i = 0;i<invoice.getReservationList().size();i++){
+				if (invoice.getReservationList().get(i).getCustomerID() == customerID){
+					invoiceList.add(invoice);
+				}
+			}
 		}
 		return invoiceList;
 	}
