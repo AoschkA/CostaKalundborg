aad6b47b8a51c82edd048c740b9960af3c8c2f45
Blackflunk <Alexander12350@hotmail.com>
2015-12-02 11:21:54 +0100

    Employee og LoginLogic lavet

--- src/logic/LoginLogic.java
+++ src/logic/LoginLogic.java
@@ -1,11 +1,24 @@
 package logic;
 
+import java.util.ArrayList;
+
+import Entity.Employee;
+
 public class LoginLogic {
+	ArrayList<Employee> employees = new ArrayList<Employee>();
+	
 	public LoginLogic(){
+		employees.add(new Employee(true, true, "ErAktiv", "ErAdmin"));
+		employees.add(new Employee(true,false, "ErAktiv","ErIkkeAdmin"));
+		employees.add(new Employee(false,true, "ErIkkeAktiv","ErAdmin"));
+		employees.add(new Employee(false,false,"ErIkkeAktiv","ErIkkeAdmin"));
 	}
 	
 	public boolean validateLogin(String username, String password){
+		for(Employee e:employees){
+			if(e.getUsername() == username && e.getPassword() == password)
+				return true;
+		}
 		return false;
-		
 	}
 }
