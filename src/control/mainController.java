052c0bfde985b4bd5065d1a398b1a60c6a2bf4c3
AoschkA <jonaspraem@gmail.com>
2015-12-02 17:30:39 +0100

    main

---
+++ src/control/mainController.java
@@ -0,0 +1,23 @@
+package control;
+
+import boundary.TUILogic;
+import logic.InvoiceLogic;
+import logic.LoginLogic;
+import logic.ReservationLogic;
+
+public class mainController {
+	private ReservationLogic reservationLogic = new ReservationLogic();
+	private InvoiceLogic invoiceLogic = new InvoiceLogic();
+	private LoginLogic loginLogic = new LoginLogic();
+	private TUILogic TUILogic = new TUILogic();
+	
+	public mainController() {
+		
+		
+	}
+	
+	
+	
+	
+
+}
