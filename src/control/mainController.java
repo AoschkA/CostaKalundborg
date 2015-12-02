3497804b5d85edd3fedafbf9b78ece8f7bf4720a
AoschkA <jonaspraem@gmail.com>
2015-12-02 18:14:28 +0100

    main

--- src/control/mainController.java
+++ src/control/mainController.java
@@ -1,6 +1,8 @@
 package control;
 
-import boundary.TUILogic;
+import entity.login.Customer;
+import entity.login.CustomerDB;
+import entity.reservation.Reservation;
 import logic.InvoiceLogic;
 import logic.LoginLogic;
 import logic.ReservationLogic;
@@ -9,13 +11,35 @@ public class mainController {
 	private ReservationLogic reservationLogic = new ReservationLogic();
 	private InvoiceLogic invoiceLogic = new InvoiceLogic();
 	private LoginLogic loginLogic = new LoginLogic();
-	private TUILogic TUILogic = new TUILogic();
+	private TUIController tuiController = new TUIController(this);
+	private CustomerDB customerDB = new CustomerDB();
 	
 	public mainController() {
+			
+	}
+	
+	public boolean userExist(String phonenumber) {
+		if (customerDB.getCustomer(phonenumber)==null) return false;
+		else return true;
+	}
+	public boolean createReservation(String phonenumber, String name, String arrivalDate, String departureDate, 
+			String type, int numChildren, int numAdults, int numDogs, boolean largeCampSite, int cottageType){
+		Customer c; 
+		if (name==null) c = customerDB.getCustomer(phonenumber);
+		else customerDB.addCustomer(name, phonenumber);
+		switch (type) {
+		case "tent": reservationLogic.makeTent(phonenumber, false, arrivalDate, departureDate, numChildren, numAdults, numDogs);
+		return true;
+		case "caravan": reservationLogic.makeCaravan(phonenumber, false, arrivalDate, departureDate, numChildren, 
+				numAdults, numDogs, largeCampSite);
+		return true;
+		case "cottage": reservationLogic.makeCottage(phonenumber, false, arrivalDate, departureDate, numAdults, cottageType);
+		return true;
+		}
+		return false;
 		
 		
 	}
-	
 	
 	
 	
--- src/entity/login/CustomerDB.java
+++ src/entity/login/CustomerDB.java
@@ -6,20 +6,19 @@ import entity.login.Customer;
 
 public class CustomerDB {
 
-	private ArrayList<Customer> customer = new ArrayList<Customer>();
+	private static ArrayList<Customer> customers;
 	
-	public CustomerDB() {}
-	
-	public void preAddedCustomer (String name, String phonenumber){
-		customer.add(new Customer("Hans Hansen", "53647584"));
-		customer.add(new Customer("Poul Jorgensen", "52834329"));
-		customer.add(new Customer("Janne Nielsen", "12542343"));
-		customer.add(new Customer ("Henning Gryn Poulsen", "98765432"));
-		customer.add(new Customer("Jannick Perkele Sorensen", "03984092"));
-		customer.add(new Customer("Ginny Germandsen", "02398420"));
-		customer.add(new Customer("Henning Poulsen", "12121212"));
+	public CustomerDB() {
+		customers=new ArrayList<Customer>();
 	}
+	
 	public void addCustomer (String name,  String phonenumber) {
-		customer.add(new Customer(name, phonenumber));
+		customers.add(new Customer(name, phonenumber));
 	}
+	public Customer getCustomer(String phonenumber){
+		for (Customer c:customers) if(c.getPhonenumber().equals(phonenumber)) return c;
+		return null;
+	}
+	
+	
 }
