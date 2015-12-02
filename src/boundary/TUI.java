13109a416c6544468ba08fc723304c129779ba1e
AoschkA <jonaspraem@gmail.com>
2015-12-02 15:49:42 +0100

    TUI

--- src/boundary/TUI.java
+++ src/boundary/TUI.java
@@ -16,7 +16,7 @@ public class TUI {
 		System.out.println("Hoved Menu");
 		System.out.println("1. Ny Reservation");
 		System.out.println("2. Slet Reservation");
-		System.out.println("3. AEndre reservation");
+		System.out.println("3. Ændre reservation");
 		System.out.println("4. Tjek ind");
 		System.out.println("5. Tjek ud");
 		
@@ -35,21 +35,21 @@ public class TUI {
 	public void deleteReservation(){
 		System.out.println("Slet Reservation");
 		System.out.println("1. Indtast reservations id");
-		System.out.println("2. Sog reservation paa kundes telefonnummer");
+		System.out.println("2. Søg reservation på kundes telefonnummer");
 		System.out.println("3. Tilbage til hovedmenu");
 	}
 	
 	public void changeReservation(){
-		System.out.println("AEndre Reservation");
+		System.out.println("Ændre Reservation");
 		System.out.println("1. Indtast reservations id");
-		System.out.println("2. Sog reservation paa kundes telefonnummer");
+		System.out.println("2. Søg reservation på kundes telefonnummer");
 		System.out.println("3. Tilbage til hovedmenu");
 	}
 	
 	public void reservationID (){
 		System.out.println("Reservation ID");
-		System.out.println("1. AEndre fra dato");
-		System.out.println("2. AEndre til dato");
+		System.out.println("1. Ændre fra dato");
+		System.out.println("2. Ændre til dato");
 		System.out.println("3. Vis reservation");
 		System.out.println("4. Fortryd og tilbage til hovedmenu");
 	}
@@ -57,7 +57,7 @@ public class TUI {
 	public void checkIn (){
 		System.out.println("Tjek Ind");
 		System.out.println("1. Indtast reservations id");
-		System.out.println("2. Sog reservation paa kundes telefonnummer");
+		System.out.println("2. Søg reservation på kundes telefonnummer");
 		System.out.println("3. Tilbage til hovedmenu");
 	}
 	
