b43f2979a684e81accd521e1a5e9256b41694b10
Blackflunk <Alexander12350@hotmail.com>
2015-12-02 13:12:40 +0100

    Invoice lavet og InvoiceLogic oprettet

--- src/logic/InvoiceLogic.java
+++ src/logic/InvoiceLogic.java
@@ -1,5 +1,40 @@
 package logic;
 
-public class InvoiceLogic {
+import java.util.ArrayList;
 
+import entity.invoice.Invoice;
+
+public class InvoiceLogic {
+	ArrayList<Invoice> invoiceList;
+	
+	public InvoiceLogic(){
+		
+	}
+	
+	public int makeInvoice(String string){
+		return 0;
+		
+	}
+	
+	public int makeInvoice(ArrayList <Invoice> invoiceList){
+		return 0;
+		
+	}
+	
+	public ArrayList<Invoice> getCustomerInvoices(String customerID){
+		for (Invoice e : invoiceList){
+			
+		}
+		return invoiceList;
+	}
+
+	public ArrayList<Invoice> getInvoices() {
+		return invoiceList;
+	}
+	
+	
+	
+	
+	
+	
 }
