e2794bfbffa2fafbc2366e0f4be88744cf303816
Tobias German <tobiasgerman@gmail.com>
2015-12-03 13:53:30 +0100

    test package med test af tests

---
+++ src/test/BookingCalenderTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import entity.reservation.BookingCalendar;
+
+public class BookingCalenderTest {
+	private static BookingCalender bc = new BookingCalendar();
+
+ public static void main(String[] args){
+	 
+ }
+}
