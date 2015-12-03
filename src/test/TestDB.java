ab7032dd54080d7d01d300ea4bbc37f1e0b5de00
STENILO <stenilo@gmail.com>
2015-12-03 17:42:14 +0100

    1

--- src/test/TestDB.java
+++ src/test/TestDB.java
@@ -9,11 +9,11 @@ public class TestDB {
 	public void positiveCustomerTest() {
 		CustomerDB customer = new  CustomerDB();
 		customer.addCustomer("Hans Hansen", "12345678");
-		customer.addCustomer("Jens Jensen", "12345678");
+		customer.addCustomer("Jens Jensen", "12345679");
 		customer.addCustomer("Hans Hansen", "87654321");
 		customer.addCustomer("Bill Jobs", "10000000");
 		assertEquals("Hans Hansen", customer.getCustomer("12345678").getName());
-		assertEquals("Jens Jensen", customer.getCustomer("12345678").getName());
+		assertEquals("Jens Jensen", customer.getCustomer("12345679").getName());
 		assertEquals("Hans Hansen", customer.getCustomer("87654321").getName());
 		assertEquals("Bill Jobs", customer.getCustomer("10000000").getName());
 		
