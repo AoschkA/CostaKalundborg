5f302cb58fb37c0b5faaaaedc2fb534b9232fbc8
Tobias German <tobiasgerman@gmail.com>
2015-12-03 13:58:53 +0100

    tests

--- src/test/BookingCalenderTest.java
+++ src/test/BookingCalenderTest.java
@@ -1,11 +1,30 @@
 package test;
 
+import java.util.ArrayList;
+
 import entity.reservation.BookingCalendar;
 
 public class BookingCalenderTest {
-	private static BookingCalender bc = new BookingCalendar();
+	private static BookingCalendar bc = new BookingCalendar();
 
- public static void main(String[] args){
-	 
- }
+	public static void main(String[] args){
+		String arrivalDate ="2015-12-12";
+		String departureDate ="2015-12-24";
+		int type = 1;
+
+		ArrayList<String> test = null;;
+		ArrayList<String> test1 = null;;
+		for(int i=0;i<1; i++){
+			test= bc.getOccupiedDays(arrivalDate, departureDate, type );
+			test1=bc.setReservation(arrivalDate, departureDate, type, 1);
+		}
+		if(!test.isEmpty()){
+			for(int i=0; i<test.size();i++){
+				System.out.println(test.get(i));
+			}
+		}
+		for(int i=0; i<test.size();i++){
+			System.out.println(test1.get(i));
+		}
+	}
 }
